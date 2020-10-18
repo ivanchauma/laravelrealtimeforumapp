ef08a7418abc0ff106cd756bfa06e254a06d00fa
Ivan Chauma <ivanchauma@gmail.com>
2020-10-18 19:58:48 +0200

    RelationShips

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     use HasFactory;
+//Every reply is under question
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function like()
+    {
+        //Every reply has many likes
+        return $this->hasMany(Like::class);
+    }
 }
